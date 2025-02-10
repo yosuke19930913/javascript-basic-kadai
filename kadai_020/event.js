aafabb1b71f1763a49820515dd33183c4c7a00df
yosuke19930913 <yosuke19930913@gmail.com>
2025-02-10 12:50:25 +0900

    kadai_20

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+ btn.addEventListener('click', () => {
+
+  const childList = document.createElement('li');
+
+  childList.textContent = 'ボタンをクリックしました';
+
+  text.textContent ="ボタンをクリックしました。"
+});
